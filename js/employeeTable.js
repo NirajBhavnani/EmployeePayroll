d47199b9bb753e5d80d749df588dabbdd2d64f75
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-09-01 00:00:10 +0530

    UC20: Search Bar implemented

--- js/employeeTable.js
+++ js/employeeTable.js
@@ -1,26 +1,35 @@
 // onload
-window.addEventListener('DOMContentLoaded', (event)=> {
+window.addEventListener("DOMContentLoaded", (event) => {
+  if (!localStorage.getItem("loginKey")) {
+    window.location.href = `./employeePayrollLogin.html?redirect=${encodeURI(
+      window.location.href
+    )}`;
+  }
 
-    if(!localStorage.getItem('loginKey')){
-        window.location.href = `./employeePayrollLogin.html?redirect=${encodeURI(window.location.href)}`
-    }
-    
-    makeAJAXCall('GET', site_properties.data_url, true).then(data => {
-        empPayReset = JSON.parse(data);
-        document.querySelector('#emp-total').textContent = empPayReset.length;//display total no. of employees
-    });
-
-    display();
+  makeAJAXCall("GET", site_properties.data_url, true).then((data) => {
+    empPayReset = JSON.parse(data);
+    document.querySelector("#emp-total").textContent = empPayReset.length; //display total no. of employees
   });
-  
-// Table creation
-async function display(){
 
-    try{
-    let EmployeePayrollList1 = await makeAJAXCall('GET', site_properties.data_url);
+  display();
+});
+
+// Table creation
+async function display() {
+  try {
+    let EmployeePayrollList1 = await makeAJAXCall(
+      "GET",
+      site_properties.data_url
+    );
     let EmployeePayrollList = JSON.parse(EmployeePayrollList1);
-        
-    let headerHTML = `<thead>
+    populateTable(EmployeePayrollList);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function populateTable(dataList) {
+  let headerHTML = `<thead>
                      <tr>
                          <th></th>
                          <th>Name</th>
@@ -30,15 +39,14 @@ async function display(){
                          <th>Start Date</th>
                          <th>Action</th>
                      </tr>
-                     </thead>`
-    
-    let innerHTML = `${headerHTML}`;
-    let employeeDataArr = EmployeePayrollList;
-    
-    
-    for(const data of employeeDataArr){
-        console.log(data);
-        innerHTML = `${innerHTML}
+                     </thead>`;
+
+  let innerHTML = `${headerHTML}`;
+  let employeeDataArr = dataList;
+
+  for (const data of employeeDataArr) {
+    console.log(data);
+    innerHTML = `${innerHTML}
                             <tbody>
                                 <tr>
                                  <td><img src= "${data.eProfile}" alt="Profile Pic"></td>
@@ -53,30 +61,24 @@ async function display(){
                                  </td>
                                 </tr>
                             </tbody>`;
-                            
-    }
-    document.querySelector('#myTable').innerHTML = innerHTML;
-}
-catch(error){
-    console.log(error);
-}
+  }
+  document.querySelector("#myTable").innerHTML = innerHTML;
 }
 
-async function deleteID(nodeId){
-    try {
-        await makeAJAXCall('DELETE', `${site_properties.data_url}/${nodeId}`, true);
-    } catch (error) {
-        console.error(error);
-    }
-    finally{
-        //refresh the page
-        display();
-    }
+async function deleteID(nodeId) {
+  try {
+    await makeAJAXCall("DELETE", `${site_properties.data_url}/${nodeId}`, true);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    //refresh the page
+    display();
+  }
 }
 
 // Redirecting to particular employee form using id
 function updateForm(nodeId) {
-    window.location.replace(`/pages/employeePayroll.html?id=${nodeId}`);
+  window.location.replace(`/pages/employeePayroll.html?id=${nodeId}`);
 }
 
 // function search(){
@@ -92,12 +94,30 @@ function updateForm(nodeId) {
 //   console.log(elementValue);
 // }
 
-// const searchBar = document.getElementById('search-bar').value;
-// searchBar.addEventListener('keyup', function(e){
-//     const term = e.target.value.toLowerCase();
-//     const emps = JSON.parse(localStorage.getItem(`EmployeeDetails`));
-//     emps = emps.filter(empData => empData.eName == term);
-//     console.log(emps);
-//     if(!emps) return;
-//     localStorage.setItem('empSearch', JSON.stringify(emps));
-// });
+document
+  .getElementById("search-bar")
+  .addEventListener("keyup", async function (e) {
+    try {
+      e.preventDefault();
+      const term = e.target.value.trim().toLowerCase();
+      var emps = JSON.parse(
+        await makeAJAXCall("GET", site_properties.data_url, true)
+      );
+      emp = emps.filter((empData) => empData.eName.toLowerCase() == term);
+      if (!emp) return;
+      localStorage.setItem("empSearch", JSON.stringify(emp));
+
+      let searchList = JSON.parse(localStorage.getItem("empSearch"));
+      if (term !== undefined) {
+        populateTable(searchList);
+        localStorage.removeItem("empSearch");
+      } else {
+        display();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    //   finally{
+    //       display();
+    //   }
+  });
